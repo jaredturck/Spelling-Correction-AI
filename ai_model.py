eb00219b1a31e80a81c99a0f9eb663eb77f32b4d
Jared Turck <jaredturck8@gmail.com>
2025-10-31 00:55:20 +0000

    Added model

--- ai_model.py
+++ ai_model.py
@@ -1,18 +1,35 @@
-import torch, random
-from torch.utils.data import Dataset
+import torch, random, time, sys
+from torch.nn import Module
+from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
+from unidecode import unidecode
 
-charset = {chr(i) : n for n,i in enumerate(range(32, 126))}
+charset = {chr(i) : n+1 for n,i in enumerate(range(32, 127))}
+UNK_ID = len(charset) + 1
+charset['<UNK>'] = UNK_ID
+
+MAX_SAMPLES = 200_000
+CONTEXT_LEN = 64
+WORD_LEN = 16
+DEVICE = 'cuda'
 
 class WikiDataset(Dataset):
     def __init__(self):
         self.training_data = []
-        self.buffer_size = 1024 * 1024 * 10  # 10 MB buffer size
+        self.buffer_size = 1024 * 1024 * 16  # 16 MB buffer size
     
     def __len__(self):
         return len(self.training_data)
     
     def __getitem__(self, idx):
         return self.training_data[idx]
+    
+    def collate_fn(self, batch):
+        x_word, x_context, y = zip(*batch)
+        x_word = pad_sequence(x_word, batch_first=True, padding_value=0)
+        x_context = pad_sequence(x_context, batch_first=True, padding_value=0)
+        y = pad_sequence(y, batch_first=True, padding_value=0)
+        return x_word, x_context, y
     
     def augment_text(self, text):
         
@@ -63,23 +80,84 @@ class WikiDataset(Dataset):
     def read_data(self):
         with open('datasets/wiki_dump_1.txt', 'r', encoding='utf-8') as file:
             file_content = '\n'
+            start = time.time()
             while file_content:
-                file_content = file.read(self.buffer_size)
+                file_content = unidecode(file.read(self.buffer_size))
                 words = file_content.split(' ')
                 seek = 0
-
+                
                 for word in words:
                     seek += len(word) + 1
-                    context_window = [charset[i] for i in file_content[max(seek - 32,0) : min(seek + 32, len(file_content))]]
-                    src_word = [charset[i] for i in self.augment_text(word)]
-                    tgt_word = [charset[i] for i in word]
+                    context_window = [charset.get(i,UNK_ID) for i in file_content[max(seek-CONTEXT_LEN//2,0) : min(seek+CONTEXT_LEN//2, len(file_content))]]
+                    src_word = [charset.get(i,UNK_ID) for i in self.augment_text(word)][:WORD_LEN]
+                    tgt_word = [charset.get(i,UNK_ID) for i in word][:WORD_LEN]
+                    self.training_data.append((torch.tensor(src_word), torch.tensor(context_window), torch.tensor(tgt_word)))
 
-                    print([src_word, context_window], tgt_word)
+                    if time.time() - start > 10:
+                        print(f'[+] Processed {len(self.training_data):,} samples')
+                        start = time.time()
+                    
+                    if len(self.training_data) >= MAX_SAMPLES:
+                        print(f'[+] Finished processing {len(self.training_data):,} samples')
+                        return
 
-                    self.training_data.append(([src_word, context_window],tgt_word))
-                    input('STOP')
+class SpellingModel(Module):
+    def __init__(self):
+        super().__init__()
+        Module.train(self, True)
+        self.dataset = WikiDataset()
+        self.dropout = 0.1
+
+        self.embedding = torch.nn.Embedding(len(charset)+1, WORD_LEN, padding_idx=0)
+        self.embedding_context = torch.nn.Embedding(CONTEXT_LEN, CONTEXT_LEN, padding_idx=0)
+        self.context_lstm = torch.nn.LSTM(input_size=CONTEXT_LEN, hidden_size=WORD_LEN, num_layers=2, batch_first=True, bidirectional=False)
+        self.main_lstm = torch.nn.LSTM(input_size=WORD_LEN, hidden_size=WORD_LEN, num_layers=6, batch_first=True, bidirectional=False)
+        self.dropout = torch.nn.Dropout(self.dropout)
+    
+    def forward(self, x):
+        src_word, context_window = x
+
+        logits = self.main_lstm(
+            self.dropout(
+                self.embedding(src_word) + self.context_lstm(self.embedding_context(context_window))
+            )
+        )
+
+        return logits
+    
+    def train_model(self):
+        self.dataset.read_data()
+        self.dataloader = DataLoader(self.dataset, batch_size=32, shuffle=True, collate_fn=self.dataset.collate_fn)
+        self.optimizer = torch.optim.AdamW(self.parameters(), lr=1e-4)
+        loss_func = torch.nn.CrossEntropyLoss()
+        start = time.time()
+
+        for epoch in range(100):
+            total_loss = 0.0
+            for n,batch in enumerate(self.dataloader):
+
+                src_word, src_context, target = batch
+                src_word.to(DEVICE)
+                src_context.to(DEVICE)
+                target.to(DEVICE)
+
+                self.optimizer.zero_grad()
+                logits = self.forward((src_word, src_context))
+                loss = loss_func(logits, target)
+                loss.backward()
+                self.optimizer.step()
+                total_loss += loss.item()
+
+                if time.time() - start > 10:
+                    print(f'[+] Batch {n+1} of {len(self.dataloader)}, loss: {loss.item()}')
+                    start = time.time()
+
+            avg_loss = total_loss / len(self.dataloader)
+            print(f'Epoch {epoch+1}, Loss: {avg_loss}')
 
 if __name__ == "__main__":
-    dataset = WikiDataset()
-    dataset.read_data()
-    
+    if len(sys.argv) > 1 and sys.argv[1] == 'train':
+        model = SpellingModel()
+        model.train_model()
+    else:
+        model = SpellingModel()
